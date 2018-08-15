90d19afdd3f3525ec678fdea9a99b2ba616b8f29
row@informatik.uni-kiel.de <Robin Wei>
2018-08-15 13:22:12 +0200

    Bamboo: Created harvester repository for the provider 'IMR'.

---
+++ src/main/java/de/gerdiproject/harvest/harvester/IMRHarvester.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright © 2018 Arnd Plumhoff (http://www.gerdi-project.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.gerdiproject.harvest.harvester;
+
+import de.gerdiproject.harvest.harvester.AbstractHarvester;
+
+
+/**
+ * A harvester for IMR (http://www.imr.no).
+ *
+ * @author Arnd Plumhoff
+ */
+public class IMRHarvester extends AbstractHarvester // TODO choose an AbstractHarvester implementation that suits your needs
+{
+    /**
+     * Default Constructor that is called by the MainContext.
+     */
+    public IMRHarvester()
+    {
+        super();
+        // TODO initialize final fields
+    }
+}
